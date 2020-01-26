4c6a8fb3ea3eddf4e73a15a0744ceec58cff4b0d
Laura Nemeth <lauranemeth.pr@gmail.com>
2020-01-26 17:41:13 -0500

    refactor: now im using hooks and also figured out the states

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
+// eslint-disable-next-line
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
+const SERVER_PATH = 'http://localhost:2020';
+
+function App(props){
+
+const [gameData, setGameData] = useState({ wordType: {}, excerpt: ""}); //actual game
+const [game, toggle] = useState(false); //on or off
+const [excerpt, setExcerpt] = useState("") // final excerpt that I show the user
+const [userInput, setUserInput] = useState({}) // what the user gives me
+
+async function fetchData(){
+  const res = await axios(SERVER_PATH + "/api/madlibs")
+  setGameData(res)
+}
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+//make button to toggle
+//on click, toggle true
+//inside of the fetch data useEffect, only fetch data if toggle is true
+//then, build a form with the fields received from the backend
+
+    return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>{gameData.data}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
