fa0f8985783be626070b35c3426bead40c342ee1
Laura Nemeth <lauranemeth.pr@gmail.com>
2020-03-03 10:00:52 -0500

    feat: start game image is a link

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
+import start from './startGame.png'
 // eslint-disable-next-line
 import axios from 'axios';
 import './App.css';
@@ -30,21 +30,19 @@ async function fetchData(){
 //font playfair display
 
     return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>{gameData.data}</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+			<div className="App">
+				<header className="App-header">
+					<a
+						className="App-link"
+						href="https://reactjs.org"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						<img src={start} className="App-logo" alt="start game" />
+					</a>
+				</header>
+			</div>
+		);
 }
 
 export default App;
