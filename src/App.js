ce40b89a9a701c9f89ad7a8a7e0fde93b0e00aa6
Laura Nemeth <lauranemeth.pr@gmail.com>
2020-03-09 21:24:16 -0400

    heyo personality change check

--- src/App.js
+++ src/App.js
@@ -1,48 +1,34 @@
 import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
 import start from './startGame.png'
+import Routes from './routes'
 // eslint-disable-next-line
-import axios from 'axios';
 import './App.css';
-
-const SERVER_PATH = 'http://localhost:2020';
 
 function App(props){
 
-const [gameData, setGameData] = useState({ wordType: {}, excerpt: ""}); //actual game
-const [game, toggle] = useState(false); //on or off
-const [excerpt, setExcerpt] = useState("") // final excerpt that I show the user
-const [userInput, setUserInput] = useState({}) // what the user gives me
+// const [gameData, setGameData] = useState({ wordType: {}, excerpt: ""}); //actual game
+// const [game, toggle] = useState(false); //on or off
+// const [excerpt, setExcerpt] = useState("") // final excerpt that I show the user
+// const [userInput, setUserInput] = useState({}) // what the user gives me
 
-async function fetchData(){
-  const res = await axios(SERVER_PATH + "/api/madlibs")
-  setGameData(res)
-}
-
-  useEffect(() => {
-    if(game === true){
-      fetchData();
-    }
-  }, []);
-
-//make button to toggle
-//on click, toggle true
-//inside of the fetch data useEffect, only fetch data if toggle is true
-//then, build a form with the fields received from the backend
-//font playfair display
 
     return (
 			<div className="App">
+			<Routes />
 				<header className="App-header">
-					<button onClick={() => toggle(true)}>
+				<Router>
+					<Link to='/game'>
 						<img
 							src={start}
 							className="App-logo"
 							alt="start game"
 							data-testid="startGame"
 						/>
-					</button>
+					</Link>
+				</Router>
 					<div>
-						<p>{gameData.data}</p>
+						<p>Hello!</p>
 					</div>
 				</header>
 			</div>
---
+++ src/components/game.js
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const SERVER_PATH = 'http://localhost:2020';
+
+
+function Game(props){
+
+const [gameData, setGameData] = useState({ wordType: {}, excerpt: '' }); //actual game
+
+async function fetchData() {
+	const res = await axios(SERVER_PATH + '/api/madlibs');
+	setGameData(res);
+}
+
+useEffect(() => {
+	fetchData();
+}, []);
+
+return(
+    <div>
+        <h1>HEHEHHEHEHEHEHE</h1>
+    </div>
+)
+
+}
+
+export default Game
