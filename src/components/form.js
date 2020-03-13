6ac63ba4457776b69d34f3fa66ced22e59b81eba
Laura Nemeth <lauranemeth.pr@gmail.com>
2020-03-13 15:49:20 -0400

    form renders correct amount of fields

--- src/components/form.js
+++ src/components/form.js
@@ -3,15 +3,36 @@ import { TextField } from '@material-ui/core'
 
 function Form(props) {
   const wordTypes = props.game.data.wordTypes
+  const wordTypeArr = objArr(wordTypes)
 
   return (
     <div>
-      {console.log(wordTypes)}
+      {console.log(wordTypeArr)}
       <form noValidate autoComplete="off">
-        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+        {wordTypeArr.map((e, idx) => {
+          return (
+            <TextField
+              key={idx}
+              id="outlined-basic"
+              label={e}
+              variant="outlined"
+            />
+          )
+        })}
       </form>
     </div>
   )
 }
 
 export default Form
+
+//helper
+const objArr = obj => {
+  let arr = []
+  for (let key in obj) {
+    for (let i = 0; i < obj[key]; i++) {
+      arr.push(key)
+    }
+  }
+  return arr
+}
