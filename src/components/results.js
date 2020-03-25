762638f5b1aa3730b61936621231a46dd016a38c
Laura Nemeth <lauranemeth.pr@gmail.com>
2020-03-25 15:29:01 -0400

    it's ALIVE

--- src/components/results.js
+++ src/components/results.js
@@ -1,13 +1,37 @@
 import React from 'react'
+import { Redirect } from 'react-router'
 
 function Results(props) {
   console.log(props)
+  const excerpt = props.location.excerpt
+  const form = props.location.form
 
-  return (
-    <div>
-      <h1>Hi</h1>
-    </div>
-  )
+  if (!props.location.excerpt && !props.location.excerpt) {
+    return <Redirect to={'/game'} />
+  } else {
+    return (
+      <div>
+        <h1>{result(form, excerpt)}</h1>
+      </div>
+    )
+  }
 }
 
 export default Results
+
+//helpers
+const result = (obj, str) => {
+  const arr = str.split(/ |\n/)
+  const regex = /[A-Z].*[A-Z]/
+  for (let i = 0; i < arr.length; i++) {
+    if (regex.test(arr[i])) {
+      for (let key in obj) {
+        if (key.toUpperCase().includes(arr[i]) && obj[key] !== '') {
+          arr[i] = obj[key]
+          obj[key] = ''
+        }
+      }
+    }
+  }
+  return arr.join(' ')
+}
