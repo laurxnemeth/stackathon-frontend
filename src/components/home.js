9ab0b1ad3901755c373a307432d214c0b92df910
Laura Nemeth <lauranemeth.pr@gmail.com>
2020-03-26 11:05:22 -0400

    home link works now

--- src/components/home.js
+++ src/components/home.js
@@ -1,14 +1,21 @@
 import React, { useState, useEffect } from 'react'
-import { BrowserRouter as Router, Link, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Link } from 'react-router-dom'
 import start from '../startGame.png'
 
 function Home(props) {
   return (
-    <Router>
-      <Link to="/game">
-        <img src={start} alt="start game" data-testid="startGame" />
-      </Link>
-    </Router>
+    <div className="home">
+      <Router>
+        <a href="/game">
+          <img
+            src={start}
+            alt="start game"
+            data-testid="startGame"
+            id="startgame"
+          />
+        </a>
+      </Router>
+    </div>
   )
 }
 
