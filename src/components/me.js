f0ae9a24e58797080e63087264ff512bbd864e85
Laura Nemeth <lauranemeth.pr@gmail.com>
2020-04-06 17:28:36 -0400

    about me, link images dont work yet, yeet yeet

--- src/components/me.js
+++ src/components/me.js
@@ -1,5 +1,29 @@
 import React from 'react'
+import thedev from '../developer.png'
+import { links } from '../links'
 
-const Me = () => {}
+const Me = () => {
+  return (
+    <div id="me">
+      <img src={thedev} alt="image of laura nemeth" id="selfie" />
+      <p>
+        {' '}
+        I’m Laura Nemeth, your friendly neighborhood developer! Other than
+        coding, I spend my free time crocheting too many sweaters, baking a
+        variety of pastries, and playing Animal Crossing. I’m passionate about
+        knowledge, information, and the accessibility of data. Hope you enjoy
+        this website, and consider getting a library card from your local
+        branch!
+      </p>
+      {links.map((link, idx) => {
+        return (
+          <div id={idx}>
+            <img src={link.img} alt={link.name} />
+          </div>
+        )
+      })}
+    </div>
+  )
+}
 
 export default Me
---
+++ src/links.js
@@ -0,0 +1,17 @@
+export const links = [
+  {
+    name: 'github link',
+    link: 'www.github.com/laurxnemeth',
+    img: '../socialMedia/github.png',
+  },
+  {
+    name: 'linkedin link',
+    link: 'www.linkedin.com/in/laurxnemeth',
+    img: '../socialMedia/linkedin.png',
+  },
+  {
+    name: 'twitter link',
+    link: 'www.twitter.com/laurxnemeth',
+    img: '../socialMedia/twitter.png',
+  },
+]
